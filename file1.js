93e373a297233cda22eb4b9a4df6e497f2df09c2
Ibraheem Khursheed <ibraheemkhursheed20@gmail.com>
2024-12-07 03:39:33 -0800

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,20 @@
+const mainButton = document.querySelector("button");
+
+const body = document.body;
+
+const current = document.querySelector(".current-color");
+
+function randomColorGenerate(){
+ const red = Math.floor(Math.random() * 256);
+ const green = Math.floor(Math.random() * 256);
+ const blue = Math.floor(Math.random() * 256);
+ const randomColor = `rgba(${red}, ${green}, ${blue})`
+ return randomColor
+}
+
+mainButton.addEventListener("click", ()=>{
+ const randomColor = randomColorGenerate();
+ body.style.backgroundColor = randomColor;
+ current.textContent = randomColor;
+ 
+})
